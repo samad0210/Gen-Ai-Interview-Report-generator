a3a40a03809fb20624a13aa7eaa99e5a155622a3
Abdul Samad Khan <khansamadabdul0202@gmail.com>
2026-04-29 00:42:33 +0530

    fix: update cors for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,27 @@ const cors = require('cors');
 const app = express();
 
 app.use(cookieParser());
+// app.use(cors({
+//     origin: 'https://gen-ai-interview-report-generator.netlify.app',
+//     credentials: true
+// }));
+
+const cors = require("cors");
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://gen-ai-interview-report-generator.netlify.app"
+];
+
 app.use(cors({
-    origin: 'https://gen-ai-interview-report-generator.netlify.app',
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
 
 
